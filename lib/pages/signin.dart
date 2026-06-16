9ba428fda8c2ee24efc6c56db869ae26028ae40d
DreiwanabeTexh <karlandreitalagtag2@gmail.com>
2026-06-16 15:04:30 +0800

    xd

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:padala_app/service/widget_support.dart';
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class createaAccount extends StatefulWidget {
   const createaAccount({super.key});
@@ -11,9 +12,16 @@ class createaAccount extends StatefulWidget {
 
 class _createaAccountState extends State<createaAccount> {
 
-    final PageController _pagecontroller = PageController();
+  final PageController _pagecontroller = PageController();
+  final TextEditingController _namecontroller = TextEditingController();
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
+  final TextEditingController _confirmpasswordcontroller = TextEditingController();
+  bool _isLoading = false;
+  String ? _errorMessage;
   int _currentPage = 0;
   late Timer _timer;
+  bool _isLogin = false;
   final List<String> _pages = [
     "assets/images/tryt.png",
     "assets/images/tryt2.png",
@@ -35,9 +43,63 @@ class _createaAccountState extends State<createaAccount> {
 
     @override
     void dispose() {
+      _namecontroller.dispose();
+      _emailcontroller.dispose();
+      _passwordcontroller.dispose();
+      _confirmpasswordcontroller.dispose();
+      _timer.cancel();
       _pagecontroller.dispose();
       super.dispose();
     }
+
+  Future<void> _handleSubmit() async {
+    setState(() {
+      _isLoading = true;
+      _errorMessage = null;
+    });
+     try {
+      if(_isLogin){
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailcontroller.text.trim(),
+          password: _passwordcontroller.text.trim(),
+        );
+        Navigator.pushReplacementNamed(context, '/home');
+      }else {
+        if(_passwordcontroller.text != _confirmpasswordcontroller.text) {
+          setState(() => _errorMessage = "Passwords do not match");
+          return;
+        } 
+        UserCredential cred = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _emailcontroller.text.trim(), password: _passwordcontroller.text);
+        await cred.user?.updateDisplayName(_namecontroller.text.trim());
+        Navigator.pushReplacementNamed(context, '/home');
+      }
+     } on FirebaseAuthException catch (e) {
+        setState(() {
+          switch (e.code) {
+            case 'user-not-found':
+              _errorMessage = "No account found with this email.";
+              break;
+            case 'wrong-password':
+              _errorMessage = "Incorrect password.";
+              break;
+            case 'email-already-in-use':
+              _errorMessage = "This email is already registered.";
+              break;
+            case 'weak-password':
+              _errorMessage = "Password must be at least 6 characters.";
+              break;
+            case 'invalid-email':
+              _errorMessage = "Please enter a valid email.";
+              break;
+            default:
+              _errorMessage = "Something went wrong. Try again.";
+          }
+        });
+      } finally {
+        setState(() => _isLoading = false);
+      }
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,8 +131,12 @@ class _createaAccountState extends State<createaAccount> {
             ),
             Align(
               alignment: Alignment.bottomRight,
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.75,  // ✅
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 400),
+                curve: Curves.easeInOut,
+                height: _isLogin 
+                ? MediaQuery.of(context).size.height * 0.52 
+                : MediaQuery.of(context).size.height * 0.73,  // ✅
                 padding:EdgeInsets.only(left: 20, right: 20, top: 25),
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -97,25 +163,55 @@ class _createaAccountState extends State<createaAccount> {
                         }),
                       ),
                       SizedBox(height: 15),
-                      Text(
-                        "Welcome to Padala App",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF2D5A27)
-                        ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: GestureDetector(
+                              onTap:() => setState(() => _isLogin = false),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: !_isLogin ? Color(0xFF2D5A27) : Colors.transparent,
+                                      width: 2.5,
+                                    ),
+                                  ),
+                                ),
+                                child: Text("Sign Up", style: AppWidget.HeadlineTextStyle(19.0).copyWith(color: !_isLogin ? Color(0xFF2D5A27) : Colors.black54), textAlign: TextAlign.center),
+                              )
+                            )
+                          ),
+                          Expanded(
+                            child: GestureDetector(
+                              onTap:() => setState(() => _isLogin = true),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: _isLogin ? Color(0xFF2D5A27) : Colors.transparent,
+                                      width: 2.5,
+                                    ),
+                                  ),
+                                ),
+                                child: Text("Login", style: AppWidget.HeadlineTextStyle(19.0).copyWith(color: _isLogin ? Color(0xFF2D5A27) : Colors.black54), textAlign: TextAlign.center),
+                              )
+                            )
+                          ),
+                        ]
                       ),
                       
-                      Container(
-                        width: double.infinity,
+                      Expanded(
                          child: SingleChildScrollView(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               SizedBox(height: 20,),
-                              Text("Create an account", style: AppWidget.HeadlineTextStyle(20.0)),
+                              Text(_isLogin ? "Welcome back!" : "Create an account", style: AppWidget.HeadlineTextStyle(20.0)),
                               SizedBox(height: 10,),
-                              Text("Fullname", style: AppWidget.SimpleTextStyle().copyWith(color: Colors.black54).copyWith(fontSize: 19)),
+                              if(!_isLogin) ...[
+                                Text("Fullname", style: AppWidget.SimpleTextStyle().copyWith(color: Colors.black54).copyWith(fontSize: 19)),
                               SizedBox(height: 10,),
                                Container(
                                   padding: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
@@ -132,6 +228,8 @@ class _createaAccountState extends State<createaAccount> {
                                 ),
 
                                 SizedBox(height: 10,),
+                              ],
+                              
                               Text("Email address", style: AppWidget.SimpleTextStyle().copyWith(color: Colors.black54).copyWith(fontSize: 19)),
                               SizedBox(height: 10,),
                                Container(
@@ -165,7 +263,8 @@ class _createaAccountState extends State<createaAccount> {
                                   )
                                 ),
 
-                                SizedBox(height: 10,),
+                                if(!_isLogin) ...[
+                                  SizedBox(height: 10,),
                               Text("Confirm Password", style: AppWidget.SimpleTextStyle().copyWith(color: Colors.black54).copyWith(fontSize: 19)),
                               SizedBox(height: 10,),
                                Container(
@@ -181,6 +280,9 @@ class _createaAccountState extends State<createaAccount> {
                                     )
                                   )
                                 ),
+                                ],
+
+                                
                                 SizedBox(height: 30,),
                               Center(
                                 child: SizedBox(
@@ -189,20 +291,17 @@ class _createaAccountState extends State<createaAccount> {
                                   child: ElevatedButton(
                                     onPressed: () {},
                                     style: ElevatedButton.styleFrom(
-                                      backgroundColor: Color.fromARGB(255, 253, 253, 253),
+                                      backgroundColor: Color(0xFF2D5A27),
                                       padding: EdgeInsets.symmetric(vertical: 15),
                                       shape: RoundedRectangleBorder(
                                         borderRadius: BorderRadius.circular(30),
-                                        side: BorderSide(color: Color(0xFF2D5A27), width: 2),
+                                        side: BorderSide(color: Color.fromARGB(255, 255, 255, 255), width: 2),
                                       ),
                                     ),
-                                    child: Text("Create Account", style: AppWidget.HeadlineTextStyle(19.0).copyWith(color: Color(0xFF2D5A27)),),
+                                    child: Text(_isLogin ? "Login" : "Create Account", style: AppWidget.HeadlineTextStyle(19.0).copyWith(color: Color.fromARGB(255, 255, 255, 255)),),
                                   ),
                                 ),
                               ),
-
-                                
-
                             ]
                           )
                          )
@@ -212,10 +311,7 @@ class _createaAccountState extends State<createaAccount> {
               ),
             ),
           ]
-        )
-      ,
-
-      
+        ),     
     );
   }
 }
